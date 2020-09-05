0f3b731225c6a447ca0ff8ea4fdb506f606c31b7
Rodrigo Pimpao <rodrigo.pimpao94@gmail.com>
2020-09-05 00:12:03 -0300

    Implement ImportTransactionsService execute().

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,45 @@
+import parse from 'csv-parse/lib/sync';
+
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
+
+interface Request {
+  fileName: string;
+  buffer: Buffer;
+  type: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute({ fileName, buffer, type }: Request): Promise<Transaction[]> {
+    console.log('reading file', fileName);
+
+    if (!type.includes('/csv')) {
+      throw new AppError('Input file must be csv.');
+    }
+
+    const transactionsToImport = parse(buffer, {
+      columns: true,
+      trim: true,
+    });
+
+    const createTransaction = new CreateTransactionService();
+
+    const transactions: Transaction[] = [];
+
+    for (let i = 0; i < transactionsToImport.length; i++) {
+      const transactionToImport = transactionsToImport[i];
+      const { title, type, value, category } = transactionToImport;
+      const transaction = await createTransaction.execute({
+        title,
+        value,
+        type,
+        categoryTitle: category,
+      });
+      transactions.push(transaction);
+    }
+
+    return transactions;
   }
 }
 
