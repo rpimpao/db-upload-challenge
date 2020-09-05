3eadac12616eacf351f40cf38fbcbbcc90286fc4
Rodrigo Pimpao <rodrigo.pimpao94@gmail.com>
2020-09-04 21:46:25 -0300

    Implement DeleteTransactionService execute().

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,24 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const entryToDelete = await transactionsRepository.findOne({ id });
+
+    if (!entryToDelete) {
+      throw new AppError('Invalid ID.');
+    }
+
+    await transactionsRepository.remove(entryToDelete);
+
+    return;
   }
 }
 
